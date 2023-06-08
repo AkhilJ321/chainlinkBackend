49f5f623b2afc3030deba16ca2e76b580a267d76
AkhilJ321 <jamwalakhil936@gmail.com>
2023-06-08 22:49:21 +0530

    Tasks Accomplished: 1.Dashboard UI added

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,20 @@ import express, { Request, Response } from "express";
 import router from "./routes";
 import morgan from "morgan";
 import dotenv from "dotenv";
+const port = 3000;
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("main");
+});
 
 app.use("/api", router);
 
